83ec4ca39f0e8918384fbc2b09c637440492d3df
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-02-05 03:28:37 +0630

    messed up, now cleaned

---
+++ 7th Pattern.py
@@ -0,0 +1,8 @@
+for i in range(6):
+    for j in range(6-i-1):
+        print(" ", end='')
+    for k in range(2 * i + 1):
+        print("*", end='')
+    for l in range(6 - i - 1):
+        print("", end='')
+    print()
