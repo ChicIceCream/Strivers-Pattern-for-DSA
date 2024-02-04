27d50a665eb894929a44d4a493d485f8d771c9c9
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-02-05 05:40:22 +0630

    another fun problem

---
+++ 10th Pattern.py
@@ -0,0 +1,9 @@
+for i in range(5):
+    for b in range(i + 1):
+        print("*", end='')
+    print()
+
+for j in range(6):
+    for e in range(5 - j - 1):
+        print("*", end="")
+    print()
